3f1de1cddd219e99922637dc3b63c15ef6a93753
Andrei <andreifrancap@gmail.com>
2024-08-31 16:39:27 -0300

    Fixed arg parser

--- inference/run_pipeline.py
+++ inference/run_pipeline.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import gi # type: ignore
 gi.require_version('Gst', '1.0')
@@ -5,42 +6,57 @@ from gi.repository import GLib, Gst # type: ignore
 from common.bus_call import bus_call
 from configs.constants import *
 from pipeline.analytics_probe import nvanalytics_src_pad_buffer_probe
-from pipeline.pipeline import create_pipeline
+from pipeline.pipeline import create_pipeline, create_rtsp_server
 
+def parse_args():
 
-frame_count = {}
-saved_count = {}
-perf_data = None
+    parser = argparse.ArgumentParser(prog="run_pipeline.py",
+                    description="EyeWay Inference Pipeline")
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="Path to input streams",
+        nargs="+",
+        metavar="URIs",
+        default=["a"],
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="display",
+        help="Output",
+        choices=["display", "file", "rtsp", "none"],
+    )
 
-
-no_display = False
-silent = False
-file_loop = False
-g_on = True
-a = None
-
-
-
-
+    # Check input arguments
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+    print(vars(args))
+    return args
 
 # Function to run the pipeline
 def run_pipeline(args):
-    pipeline, analytics, perf_data = create_pipeline(args)
+    args = parse_args()
+
+    create_rtsp_server()
+    pipeline, analytics, perf_data, element_probe = create_pipeline(args)
     if not pipeline:
         sys.stderr.write("Failed to create pipeline\n")
         return
-
 
     loop = GLib.MainLoop()
     bus = pipeline.get_bus()
     bus.add_signal_watch()
     bus.connect ("message", bus_call, loop)
 
-    nvanalytics_src_pad=analytics.get_static_pad("src")
-    if not nvanalytics_src_pad:
+    element_probe=element_probe.get_static_pad("src")
+    if not element_probe:
         sys.stderr.write(" Unable to get src pad \n")
     else:
-        nvanalytics_src_pad.add_probe(Gst.PadProbeType.BUFFER, nvanalytics_src_pad_buffer_probe, 0, perf_data)
+        element_probe.add_probe(Gst.PadProbeType.BUFFER, nvanalytics_src_pad_buffer_probe, 0, perf_data)
         # perf callback function to print fps every 5 sec
         GLib.timeout_add(5000, perf_data.perf_print_callback)
 
