6b14b94c78bacda7ec574fb7cb53cb09f00cb893
Andrei <andreifrancap@gmail.com>
2024-10-24 21:57:49 -0300

    added hls output path

--- inference/configs/constants.py
+++ inference/configs/constants.py
@@ -2,9 +2,9 @@ PGIE_CLASS_ID_VEHICLE = 0
 PGIE_CLASS_ID_BICYCLE = 1
 PGIE_CLASS_ID_PERSON = 2
 PGIE_CLASS_ID_ROADSIGN = 3
-MUXER_BATCH_TIMEOUT_USEC = 33000
-TILED_OUTPUT_WIDTH=854
-TILED_OUTPUT_HEIGHT=480 
+MUXER_BATCH_TIMEOUT_USEC = 0
+TILED_OUTPUT_WIDTH=1280
+TILED_OUTPUT_HEIGHT=720 
 OSD_PROCESS_MODE= 0
 OSD_DISPLAY_TEXT= 1
 
@@ -14,3 +14,6 @@ MAX_CONFIDENCE = 0.4
 PGIE_CONFIG_FILE = "/opt/nvidia/deepstream/deepstream-7.0/sources/apps/inference/configs/config_pgie_yolo_det.txt"
 ANALYTICS_CONFIG_FILE = "/opt/nvidia/deepstream/deepstream-7.0/sources/apps/inference/configs/config_nvdsanalytics.txt"
 TRACKER_CONFIG_FILE = "/opt/nvidia/deepstream/deepstream-7.0/sources/apps/inference/configs/dsnvanalytics_tracker_config.txt"
+
+
+HLS_OUTPUT_PATH = "/opt/nvidia/deepstream/deepstream-7.0/sources/apps/inference/stream"
